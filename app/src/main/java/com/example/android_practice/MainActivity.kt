ddfb7af13b76e815cbb648b3653fe670b607d641
LEE KYU WON <duffufk1202@naver.com>
2021-06-09 19:09:31 +0900

    Use toast

---
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android_practice
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "Button pushed", Toast.LENGTH_LONG).show()
+        }
+    }
+}
